eaa8bc3589ee4b207610795cfc2fb720da25c598
Saul Solano <saul3d@yahoo.com>
2019-07-02 16:20:08 -0500

    created setup and auth

---
+++ src/Component/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="homeComponent">
+        <button className="btn btn-danger">button</button>
+      </div>
+    );
+  }
+
+}
+
+export default Home;
